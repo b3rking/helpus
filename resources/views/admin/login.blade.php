b498fe98e592f1c7ca5eb056080f5633450577c9
b3rking <ishimweeschylebersy@gmail.com>
2021-05-12 08:46:54 +0200

    fixed the margin

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -2,16 +2,17 @@
 @section('title', 'Page de connection')
 @section('content')
     <div class="bg-blue-200 p-2 m-2 mb-4 mx-auto text-center text-blue-900 ">cette page est reservé aux administrateur du site. pour les visiteur <a href="{{ route('home') }}" class="text-lg text-gray-900">cliquer ici</a></div>
-    <div class="container mx-auto border-2 rounded-sm w-80 ">
-        <h2 class="text-center capitalize text-xl font-bold">identifiez vous</h2>
+    <div class="container mx-auto border-2 rounded-lg w-80 ">
+        <h2 class="text-center capitalize text-xl font-bold m-4">identifiez vous</h2>
         <form action="" method="GET" autocomplete="off">
             <div class="box">
                 <span class="icon-style px-2"><i class="fas fa-user"></i></span>
-                <input type="text" name="username" class="input">
+                <input type="text" name="username" class="px-2 focus:outline-none my-3 border-2 py-2" placeholder="nom d'utilisateur">
             </div>
-            <div class="box">
-                <span class="icon-style pr-2"><i class="fas fa-lock"></i></span>
-                <input type="password" name="password" class="input">
+
+            <div class="box mx-auto">
+                <span class="icon-style px-2"><i class="fas fa-lock"></i></span>
+                <input type="password" name="password" class="px-2 focus:outline-none my-3 bg-transparent py-2" placeholder="******">
             </div>
         </form>
     </div>
