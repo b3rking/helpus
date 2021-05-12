6eb98c25d04bbd4ad17e3468d97190eba4ec7838
b3rking <ishimweeschylebersy@gmail.com>
2021-05-12 11:02:48 +0200

    changed the url to assets

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Helpus - @yield('title', 'home page')</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
-    <link rel="manifest" href="favicons/site.webmanifest">
-    <link rel="shortcut icon" type="image/png" href="favicons/favicon.ico">
-    <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('favicons/site.webmanifest') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('favicons/favicon.ico') }}">
+    <link rel="mask-icon" href="{{ asset('favicons/safari-pinned-tab.svg') }}" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#000000">
     <script src="https://kit.fontawesome.com/29e3c799c5.js" crossorigin="anonymous"></script>
--- resources/views/client/form.blade.php
+++ resources/views/client/form.blade.php
@@ -1,3 +1,19 @@
 @extends('app')
-@section('title', 'Nouveau Admin')
+@section('title', "Nouveau Admin")
+@section('content')
+<form action="/register" method="POST" class="">
+    <input type="text" name="" id="">
+</form>
+@endsection
 
+
+
+/**
+
+('telephone')->nullable();
+            $table->string('fullname');
+            $table->string('username');
+            $table->string('password');
+
+
+*/
