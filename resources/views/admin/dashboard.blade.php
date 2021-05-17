234de15394d44621ccec14cf4d308393c25b26a7
b3rking <ishimweeschylebersy@gmail.com>
2021-05-17 10:36:53 +0200

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,8 @@
 @extends('app')
 @section('title', "Page d'administration")
 @section('content')
-    <h2 class="text-green-600 text-lg capitalize text-center">dashboard</h2>
+    <div class="container text-center bg-blue-600 w-screen flex flex-row">
+        <div class="bg-green-600  h-5"></div>
+        <div class="bg-red-500 h-5"></div>
+    </div>
 @stop
