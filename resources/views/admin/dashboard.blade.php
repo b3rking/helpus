c7cff93c7e262addae2e59d529802287d59c2df6
b3rking <ishimweeschylebersy@gmail.com>
2021-05-23 20:50:14 +0200

    working on dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,8 +1,17 @@
 @extends('app')
 @section('title', "Page d'administration")
 @section('content')
-    <div class="container text-center bg-blue-600 w-screen flex flex-row">
-        <div class="bg-green-600  h-5"></div>
-        <div class="bg-red-500 h-5"></div>
+    <div class="text-center bg-blue-600 w-screen flex flex-row">
+        <div class="bg-green-600  h-auto w-36 side my-10">
+            <ul>
+                <li class="capitalize py-2 text-md "><a href="">items 1</a></li>
+                <li class="capitalize py-2 text-md "><a href="">items 2</a></li>
+                <li class="capitalize py-2 text-md "><a href="">items 3</a></li>
+                <li class="capitalize py-2 text-md "><a href="">items 4</a></li>
+            </ul>
+        </div>
+        <div class="bg-red-500 h-auto w-11/12 main">
+            <h2 class="text-lg">working as {{ $user->username }}</h2>
+        </div>
     </div>
 @stop
