7e4d5476253ff761582f719a5aeb867bf9c783d8
b3rking <ishimweeschylebersy@gmail.com>
2021-05-23 20:59:14 +0200

    styled the side bar

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,17 @@
 @extends('app')
 @section('title', "Page d'administration")
 @section('content')
-    <div class="text-center bg-blue-600 w-screen flex flex-row">
-        <div class="bg-green-600  h-auto w-36 side my-10">
+    <div class="text-center w-screen flex flex-row">
+        <div class="px-2 h-auto w-36 side my-10">
             <ul>
-                <li class="capitalize py-2 text-md "><a href="">items 1</a></li>
-                <li class="capitalize py-2 text-md "><a href="">items 2</a></li>
-                <li class="capitalize py-2 text-md "><a href="">items 3</a></li>
-                <li class="capitalize py-2 text-md "><a href="">items 4</a></li>
+                <li class="capitalize py-2 text-md font-semibold hover:bg-gray-300 rounded-lg"><a href="">items 1</a></li>
+                <li class="capitalize py-2 text-md font-semibold hover:bg-gray-300 rounded-lg"><a href="">items 2</a></li>
+                <li class="capitalize py-2 text-md font-semibold hover:bg-gray-300 rounded-lg"><a href="">items 3</a></li>
+                <li class="capitalize py-2 text-md font-semibold hover:bg-gray-300 rounded-lg"><a href="">items 4</a></li>
             </ul>
         </div>
         <div class="bg-red-500 h-auto w-11/12 main">
-            <h2 class="text-lg">working as {{ $user->username }}</h2>
+            <h2 class="text-lg font-bold capitalize underline">bienvenue {{ $user->username }}</h2>
         </div>
     </div>
 @stop
