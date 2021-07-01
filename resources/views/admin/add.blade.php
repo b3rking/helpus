8e0d2c5129f571258e61afffd353cb4289064ef7
ISHIMWE ESCHYLE BERSY <ishimweeschylebersy@gmail.com>
2021-07-01 10:28:48 +0200

    new template file

---
+++ resources/views/admin/add.blade.php
@@ -0,0 +1,5 @@
+@extends('app')
+@section('title', "Page d'administration")
+@section('content')
+    <h1 class="capitalize text-xl font-semibold text-center text-gray-500 my-4">ajouter un nouveau patient</h1>
+@stop
