6f7471459aea5d27d16606de1bf50ed57696a7d8
ISHIMWE ESCHYLE BERSY <ishimweeschylebersy@gmail.com>
2021-07-05 10:23:02 +0200

    updated the form structure

--- resources/views/admin/add.blade.php
+++ resources/views/admin/add.blade.php
@@ -3,54 +3,76 @@
 @section('content')
     <h1 class="capitalize text-xl font-semibold text-center text-gray-500 my-4">ajouter un nouveau patient</h1>
     <div class="">
-        <form action="" method="post">
+        <form action="" method="post" class="w-11/12 mx-auto">
             @csrf
-            <label for="nom&prenom">
-                <p>nom et prenom</p>
-                <input type="text" name="nom&prenom">
-            </label>
-            <label for="adress">
-                <p>adresse</p>
-                <input type="text" name="adress">
-            </label>
-            <label for="story">
-                <p>histoire</p>
-                <input type="text" name="story">
-            </label>
-            <label for="pic_path">
-                <p>photo du malade</p>
-                <input type="text" name="pic_path">
-            </label>
-            <label for="etat">
-                <p>etat</p>
-                <select>
-                    <option value="">grave</option>
-                </select>
-            </label>
-            <label for="ecocash_name">
-                <p>nom ecocash</p>
-                <input type="text" name="ecocash_name">
-            </label>
-            <label for="ecocash_number">
-                <p>numero ecocash</p>
-                <input type="number" name="ecocash_number">
-            </label>
-            <label for="lumicash_name">
-                <p>nom lumicash</p>
-                <input type="text" name="lumicash_name">
-            </label>
-            <label for="lumicash_number">
-                <p>numero lumicash</p>
-                <input type="number" name="lumicash_number">
-            </label>
-            <label for="bank_name">
-                <p>nom de la bank</p>
-                <input type="text" name="bank_name">
-            </label>
-            <label for="bank_number">
-                <p>numero du compte</p>
-                <input type="number" name="bank_number">
-            </label>
+            <div class="my-4">
+                <label for="nom&prenom">
+                    <p class="font-semibold capitalize">nom et prenom</p>
+                    <input type="text" name="nom&prenom" class="border-2 border-gray-700 rounded-md w-1/5 p-1">
+                </label>    
+            </div>
+            <div class="my-4">
+                <label for="adress" class="">
+                    <p>adresse</p>
+                    <input type="text" name="adress">
+                </label>
+            </div>
+            <div class="my-4">
+                <label for="story">
+                    <p>histoire</p>
+                    <input type="text" name="story">
+                </label>
+            </div>
+            <div class="my-4">
+                <label for="pic_path">
+                    <p>photo du malade</p>
+                    <input type="text" name="pic_path">
+                </label>
+            </div>
+            <div class="my-4">
+                <label for="etat">
+                    <p>etat</p>
+                    <select>
+                        <option value="">grave</option>
+                    </select>
+                </label>
+            </div>
+            <div class="my-4">
+                <label for="ecocash_name">
+                    <p>nom ecocash</p>
+                    <input type="text" name="ecocash_name">
+                </label>
+            </div>
+            <div class="my-4">
+                <label for="ecocash_number">
+                    <p>numero ecocash</p>
+                    <input type="number" name="ecocash_number">
+                </label>
+            </div>
+            <div class="my-4">
+                <label for="lumicash_name">
+                    <p>nom lumicash</p>
+                    <input type="text" name="lumicash_name">
+                </label>
+            </div>
+            <div class="my-4">
+                <label for="lumicash_number">
+                    <p>numero lumicash</p>
+                    <input type="number" name="lumicash_number">
+                </label>
+            </div>
+            <div class="my-4">
+                <label for="bank_name">
+                    <p>nom de la bank</p>
+                    <input type="text" name="bank_name">
+                </label>
+            </div>
+            <div class="my-4">
+                <label for="bank_number">
+                    <p>numero du compte</p>
+                    <input type="number" name="bank_number">
+                </label>
+            </div>
         </form>
     </div>
 @stop
