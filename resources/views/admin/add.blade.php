8034bc4ae64ada4253fec585771ccb643232389b
ISHIMWE ESCHYLE BERSY <ishimweeschylebersy@gmail.com>
2021-07-05 10:01:04 +0200

    created the formular for adding to patient

--- resources/views/admin/add.blade.php
+++ resources/views/admin/add.blade.php
@@ -5,7 +5,52 @@
     <div class="">
         <form action="" method="post">
             @csrf
-
+            <label for="nom&prenom">
+                <p>nom et prenom</p>
+                <input type="text" name="nom&prenom">
+            </label>
+            <label for="adress">
+                <p>adresse</p>
+                <input type="text" name="adress">
+            </label>
+            <label for="story">
+                <p>histoire</p>
+                <input type="text" name="story">
+            </label>
+            <label for="pic_path">
+                <p>photo du malade</p>
+                <input type="text" name="pic_path">
+            </label>
+            <label for="etat">
+                <p>etat</p>
+                <select>
+                    <option value="">grave</option>
+                </select>
+            </label>
+            <label for="ecocash_name">
+                <p>nom ecocash</p>
+                <input type="text" name="ecocash_name">
+            </label>
+            <label for="ecocash_number">
+                <p>numero ecocash</p>
+                <input type="number" name="ecocash_number">
+            </label>
+            <label for="lumicash_name">
+                <p>nom lumicash</p>
+                <input type="text" name="lumicash_name">
+            </label>
+            <label for="lumicash_number">
+                <p>numero lumicash</p>
+                <input type="number" name="lumicash_number">
+            </label>
+            <label for="bank_name">
+                <p>nom de la bank</p>
+                <input type="text" name="bank_name">
+            </label>
+            <label for="bank_number">
+                <p>numero du compte</p>
+                <input type="number" name="bank_number">
+            </label>
         </form>
     </div>
 @stop
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,22 +4,34 @@
     <div class="text-center w-screen flex flex-row">
         <div class="px-2 h-auto w-36 side my-10">
             <ul>
-                <li class="admin-link"><a href="">ajouter <i class="fas fa-plus"></i></a></li>
+                <li class="admin-link"><a href="{{ route('patient.create') }}">Ajouter <i class="fas fa-plus"></i></a></li>
                 <li class="admin-link"><a href="">items 2</a></li>
                 <li class="admin-link"><a href="">items 3</a></li>
                 <li class="admin-link"><a href="">items 4</a></li>
             </ul>
         </div>
-        <div class="bg-red-500 h-auto w-11/12 main">
-            <h2 class="text-lg font-bold capitalize">bienvenue {{ $user->username }}</h2>
-            <div class="bg-yellow-500 h-8 w-8">
-                <table class="table-auto">
+        <div class="h-auto w-11/12 main">
+            <h2 class="text-lg font-bold capitalize">Tous les malades</h2>
+            <div class="h-screen w-screen">
+                <table class="table-auto mx-auto">
                     <thead>
                         <th>nom & prenom</th>
                         <th>adress</th>
                         <th>tel</th>
+                        <th>actions</th>
                     </thead>
                     <tbody>
+                        <tr>
+                            <td class="px-6">ISHIMWE Eschyle Bersy</td>
+                            <td class="px-6">kanyosha, 8av n°45</td>
+                            <td class="px-6">78569321</td>
+                            <td class="px-6"><a href="">del</a><a href="">edit</a></td>
+                        </tr>
+                        <tr>
+                            <td>ISHIMWE Eschyle Bersy</td>
+                            <td>kanyosha, 8av n°45</td>
+                            <td>78569321</td>
+                        </tr>
                         <tr>
                             <td>ISHIMWE Eschyle Bersy</td>
                             <td>kanyosha, 8av n°45</td>
