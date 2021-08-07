8e8b5fe27e36cd98391f5d414feb980beb4bc851
b3rking <ishimweeschylebersy@gmail.com>
2021-08-07 21:14:49 +0200

    started working on home page

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 @section('title', "Page d'accueil")
 @section('content')
-    <p class="p-8 text-yellow-400 font-extrabold">je suis a la page d'acceuil!</p>
+    <h1>All patients</h1>
     @if(count($patients) <= 0)
         no patient yet!
     @else
