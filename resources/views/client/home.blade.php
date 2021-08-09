c371899a6d705f54382d6b8a23531267ef60ae95
b3rking <ishimweeschylebersy@gmail.com>
2021-08-09 18:41:19 +0200

    added the pic

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -6,13 +6,9 @@
         <p class="pl-4 capitalize text-lg">Aucun malade pour l'instant...</p>
     @else
         @foreach($patients as $pt)
-
-            <div>
-                
+            <div class="p-4">
+                <img src="./images/lo.jpg" alt="photo du malade" class="w-32">
             </div>
-
-
-            <img src="{{ $pt->pic_adress }}" alt="{{ $pt->fullname }} picture at" >
         @endforeach
     @endif
 @endsection
