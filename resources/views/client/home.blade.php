c1944a81df00a77ba9670bcee6761e56d491e5e0
b3rking <ishimweeschylebersy@gmail.com>
2021-08-09 19:12:36 +0200

    done the basic design

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -6,11 +6,11 @@
         <p class="pl-4 capitalize text-lg">Aucun malade pour l'instant...</p>
     @else
         @foreach($patients as $pt)
-            <div class="p-4 flex flex-row">
-                <img src="./images/lo.jpg" alt="photo du malade" class="w-32">
+            <div class="p-2 flex flex-row">
+                <img src="./images/lo.jpg" alt="photo du malade" class="w-24">
                 <div class="flex flex-col items-center justify-center">
-                    <h2>{{ $pt->fullname }}</h2>
-                    <p>
+                    <h2 class="capitalize text-lg font-semibold text-left ">{{ $pt->fullname }}</h2>
+                    <p class="w-11/12 p-2">
                         {!! $pt->story !!}
                         <a href="{{ route("detail") }}">plus de detail</a>
                     </p>
